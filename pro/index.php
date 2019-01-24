58b9f6e7eb46eb3a775dc5ed2a8c70c826ce4b72
Rehan Abid <rehan_abid@ucp.edu.pk>
2019-01-24 14:25:03 +0500

    Quiz # 8 completed

--- pro/index.php
+++ pro/index.php
@@ -30,7 +30,7 @@ require "server/functions.php";
                         <form class="form-inline">
                             <div class="input-group">
                                 <input type="search" class="form-control"
-                                       id="search-bar" name="search"
+                                       id="search-bar" name="search" onkeyup="update(this.value)"
                                        placeholder="Find Mobile Phones, Laptops, and more..">
                                 <div class="input-group-append">
                                     <button class="btn btn-outline-secondary btn-lg" type="submit"><i class="fas fa-search"></i></button>
@@ -95,7 +95,7 @@ require "server/functions.php";
     </nav>
     <article id="content" class="container-fluid bg-white">
 
-        <div class="row">
+        <div id="mainBody" class="row">
                 <?php getPro(); ?>
         </div>
     </article>
@@ -111,5 +111,20 @@ require "server/functions.php";
     </footer>
 <script src="js/jquery-3.3.1.js"></script>
 <script src="js/bootstrap.bundle.js"></script>
+<script>
+    function update(i) {
+        if(i.length>0)
+        {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("mainBody").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET", "check_search.php?e=" + i, true);
+            xmlhttp.send();
+        }
+    }
+</script>
 </body>
 </html>
